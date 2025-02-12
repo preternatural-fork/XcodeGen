74a9b2efb12523e7a4644b3f5878a55c0e232d47
Vatsal Manot <vatsal.manot@yahoo.com>
2025-02-12 04:31:09 -0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -16,10 +16,10 @@ let package = Package(
         .package(url: "https://github.com/yonaskolb/JSONUtilities.git", from: "4.2.0"),
         .package(url: "https://github.com/kylef/Spectre.git", from: "0.9.2"),
         .package(url: "https://github.com/onevcat/Rainbow.git", from: "4.0.0"),
-        .package(url: "https://github.com/tuist/XcodeProj.git", exact: "8.16.0"),
+        .package(url: "https://github.com/tuist/XcodeProj.git", from: "8.16.0"),
         .package(url: "https://github.com/jakeheis/SwiftCLI.git", from: "6.0.3"),
         .package(url: "https://github.com/mxcl/Version", from: "2.0.0"),
-        .package(url: "https://github.com/freddi-kit/ArtifactBundleGen", exact: "0.0.6")
+        .package(url: "https://github.com/freddi-kit/ArtifactBundleGen", from: "0.0.6")
     ],
     targets: [
         .executableTarget(name: "XcodeGen", dependencies: [
